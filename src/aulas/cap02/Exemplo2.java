dcd5158ff0dc5077ed58db8d498e94e5c0ba5b24
Danilocg07 <danilo-c-g@hotmail.com>
2021-08-08 15:56:01 -0300

    Cap 02 - Exemplos

---
+++ src/aulas/cap02/Exemplo2.java
@@ -0,0 +1,11 @@
+package aulas.cap02;
+
+public class Exemplo2 {
+    public static void main(String[] args) {
+       
+        int matricula = 1089, idade = 20;//declaração de variavéis do tipo inteiro
+        double dolar = 2.62;
+       /* exemplo de comentário aqui no java*/
+        System.out.println("matricula: "+ matricula + " idade: "+ idade + " dolar: "+ dolar);
+    }
+}
